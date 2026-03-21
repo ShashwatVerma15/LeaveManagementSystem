1ac011b1a02797bc6490a169704a62b8af412f53
Shashwat <shashwatv@yugensys.com>
2026-03-21 19:05:40 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DOB = new DateTime(2000,12,25)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DOB { get; set; }
+    }
+}
